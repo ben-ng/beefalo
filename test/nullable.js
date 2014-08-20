97dd9e2521afe542cd228bb69505db0179bd8c94
Ben Ng <me@benng.me>
2014-08-19 21:31:56 -0700

    Cover nullable

--- test/nullable.js
+++ test/nullable.js
@@ -4,9 +4,10 @@ var test = require('tape')
   , nullable = require('../lib/nullable')
 
 test('nullable', function (t) {
-  t.plan(2)
+  t.plan(3)
 
   var nullableTable = nullable(simpleGrammar)
+    , specialGrammar
 
   t.deepEqual(nullableTable
     , [ [ true, true, true ], [ true, true, false, true, false ], {c: false} ]
@@ -20,4 +21,24 @@ test('nullable', function (t) {
     , {A: false, 'A C': false, B: false, 'B C': false, C: false} ]
     , 'Should return two nullable tables, one for NTs, one for RHS, ' +
       'one for every sequence after a NT, all false')
+
+  specialGrammar = {
+    nonTerminals: ['T', 'A', 'B', 'C']
+  , terminals: ['c']
+  , rules: [
+      ['T', ['A', 'B']] // Nullable
+    , ['T', ['A', 'B', 'C']] // Not Nullable
+    , ['A', []]
+    , ['B', []]
+    , ['C', ['c']]
+    ]
+  }
+
+  nullableTable = nullable(specialGrammar)
+
+  t.deepEqual(nullableTable
+    , [ [ false, true, true, false ],
+      [ true, false, true, true, false ],
+      { B: true, 'B C': false, C: false } ]
+    , 'Should know concatenated nullable tokens are still nullable')
 })
